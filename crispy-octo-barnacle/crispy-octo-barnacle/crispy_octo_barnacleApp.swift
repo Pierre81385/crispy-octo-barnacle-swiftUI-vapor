d2dbc7a305a8ce4fcdf742e78396fa284016a598
Peter Bishop <pjb.den@gmail.com>
2024-01-03 22:51:08 -0700

    basic xcode project created

---
+++ crispy-octo-barnacle/crispy-octo-barnacle/crispy_octo_barnacleApp.swift
@@ -0,0 +1,17 @@
+//
+//  crispy_octo_barnacleApp.swift
+//  crispy-octo-barnacle
+//
+//  Created by Peter Bishop on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct crispy_octo_barnacleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
